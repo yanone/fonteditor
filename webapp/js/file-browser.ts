e4807b8857e9fda940f7bdcb305863a31cca65dd
Yanone <post@yanone.de>
2026-03-02 17:38:23 +0100

    File size in ceil(kB)

--- webapp/js/file-browser.ts
+++ webapp/js/file-browser.ts
@@ -284,11 +284,8 @@ async function maybeReloadCurrentFontFromDisk(
 }
 
 function formatFileSize(bytes: number): string {
-    if (bytes === 0) return '0 B';
-    const k = 1024;
-    const sizes = ['B', 'KB', 'MB', 'GB'];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(1)) + ' ' + sizes[i];
+    if (bytes === 0) return '0 kB';
+    return `${Math.max(1, Math.ceil(bytes / 1024))} kB`;
 }
 
 function truncatePathMiddle(
